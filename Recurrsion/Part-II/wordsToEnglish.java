a72733d923aeb8ec1665157f01e5b6b13fe9b32c
AlokRanjan-github <alokrp7007@gmail.com>
2024-10-21 10:27:27 +0530

    recursion modif

--- Recurrsion/Part-II/wordsToEnglish.java
+++ Recurrsion/Part-II/wordsToEnglish.java
@@ -1,22 +1,15 @@
 public class wordsToEnglish {
-    public static String toEnglish(int n) {
+    public static void toEnglish(int n) {
         String[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-        if (n < 10) {
-            return words[n];
+        if (n == 0) {
+            return ;
         }
-        return toEnglish(n / 10) + " " + words[n % 10];
-
+        int lastDigit = n%10;
+        toEnglish(n/10);
+        System.out.println(words[lastDigit]);
     }
-
-    /*
-     * First call: numberToWords(2019) → calls numberToWords(201) and adds "nine".
-     * Second call: numberToWords(201) → calls numberToWords(20) and adds "one".
-     * Third call: numberToWords(20) → calls numberToWords(2) and adds "zero".
-     * Base case: numberToWords(2) returns "two".
-     */
-
     public static void main(String[] args) {
         int num = 2019;
-        System.out.println(toEnglish(num));
+        toEnglish(num);
     }
 }
