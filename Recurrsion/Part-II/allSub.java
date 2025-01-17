0c6c7c80df163297956a6a901e1acae65e13d7bf
AlokRanjan-github <alokrp7007@gmail.com>
2025-01-17 11:16:16 +0530

    LL basic operations

--- Recurrsion/Part-II/allSub.java
+++ Recurrsion/Part-II/allSub.java
@@ -1,12 +1,15 @@
 public class allSub {
+    static int count=0;
     public static void length(String str,int i){
         if (i == str.length()-1) {
-            System.out.print(str.charAt(i));
+            count++;
+            System.out.print(str.charAt(i)+"\n");
             return;
         }
         for (int j = i; j < str.length(); j++) {
             String newStr = new String("");
             if (str.charAt(i) == str.charAt(j)) {
+                count++;
                 newStr = str.substring(i, j+1);
                 System.out.print(newStr+" ");
             }
@@ -14,6 +17,7 @@ public class allSub {
         length(str, i+1);
     }
     public static void main(String[] args) {
-        length("aba", 0);
+        length("abcab", 0);
+        System.out.println(count);
     }
 }
