a4298c9ca4ef8dd0f3866c7f3f457bb7efa3ac41
AlokRanjan-github <alokrp7007@gmail.com>
2025-02-23 16:12:23 +0530

    Stacks using 2 Qs

--- Queues/Qusing2stacks.java
+++ Queues/Qusing2stacks.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+//Using Efficient pop&peek O(n) and costly push
 public class Qusing2stacks {
     Stack<Integer> s1 = new Stack<>();
     Stack<Integer> s2 = new Stack<>();
@@ -7,7 +8,7 @@ public class Qusing2stacks {
     public boolean isEmpty() {
         return s1.isEmpty();
     }
-
+    // Costly push operation 
     public void add(int data) {
         while (!s1.isEmpty()) {
             s2.push(s1.pop());
@@ -51,7 +52,71 @@ public class Qusing2stacks {
         // q.remove();
         // q.peek();
         q.removeAllElementsOfQ();
-        
+
     }
 
+}
+
+
+
+
+//Using efficent Push O(n) and costly remove&peek
+public class Qusing2stacks {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public boolean isEmpty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+
+    public void add(int data) {
+        s1.push(data);
+        System.out.println("Added: " + data);
+    }
+
+    public void remove() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return;
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        System.out.println("Removed: " + s2.pop());
+
+    }
+
+    public void peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return;
+        }
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        System.out.println("Peeked: " + s2.peek());
+    }
+
+    public static void main(String[] args) {
+        Qusing2stacks q = new Qusing2stacks();
+        q.add(50);
+        q.add(60);
+        q.add(70);
+        q.add(80);
+        q.add(90);
+        q.peek();
+        q.remove();;
+        q.peek();
+        q.remove();
+        q.remove();
+        q.peek();
+        q.remove();
+        q.remove();
+        q.peek();
+    }
 }
