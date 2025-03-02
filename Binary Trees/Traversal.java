fff893f10d030f73efc98334e29dbcd9c0bd2067
AlokRanjan-github <alokrp7007@gmail.com>
2025-03-02 10:30:26 +0530

    Height of tree

--- Binary Trees/Traversal.java
+++ Binary Trees/Traversal.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Traversal {
 
@@ -13,7 +15,7 @@ public class Traversal {
         }
     }
 
-    static int index = -1; 
+    static int index = -1;
 
     public static Node addNode(int[] nodes) {
         index++;
@@ -28,43 +30,67 @@ public class Traversal {
         return newNode;
     }
 
-    public static void preOrder(Node root){
+    public static void preOrder(Node root) {
         if (root == null) {
             return;
         }
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         preOrder(root.left);
         preOrder(root.right);
     }
 
-    public static void inOrder(Node root){
+    public static void inOrder(Node root) {
         if (root == null) {
             return;
         }
         inOrder(root.left);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         inOrder(root.right);
     }
 
-    public static void postOrder(Node root){
+    public static void postOrder(Node root) {
         if (root == null) {
             return;
         }
         postOrder(root.left);
         postOrder(root.right);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
     }
 
-    public static void levelOrder(Node root){
-        
-    }
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
 
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, -1}; 
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1 };
 
         Node root = addNode(nodes);
-
+ 
         if (root != null) {
             System.out.println("Root node value: " + root.data);
         } else {
@@ -79,7 +105,7 @@ public class Traversal {
 
         System.out.println("\nPostOrder Traversal:");
         postOrder(root);
-        
+
         System.out.println("\nlevelOrder Traversal:");
         levelOrder(root);
 
